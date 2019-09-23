8c16511287df5e87c9840fa683822e061e1c2db4
Eason <eason.yty@gmail.com>
2019-09-23 11:09:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const addBtn = document.querySelector('.add-button');
 window.addEventListener('beforeinstallprompt', (e) => {
   
   console.log("printing event:" + e); 
+  console.log("printing event:" + e.platforms); 
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
